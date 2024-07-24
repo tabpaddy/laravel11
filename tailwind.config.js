917a12886b71d8fc30fc5d6c45cdd57eab38dc08
smallboyintech <111036308+tabpaddy@users.noreply.github.com>
2024-07-24 21:57:27 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    //darkMode: 'class', // or 'media' for automatic dark mode based on user's system settings
+    content: [
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+      "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+
